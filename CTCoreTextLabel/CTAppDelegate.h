b73fff101f1d228f63a83ce9b5d8fc8299d443dd
j_chen <j_chen@DST55721.local>
2013-12-02 15:13:14 +0800

    Initial Commit

---
+++ CTCoreTextLabel/CTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CTAppDelegate.h
+//  CTCoreTextLabel
+//
+//  Created by j_chen on 13-12-2.
+//  Copyright (c) 2013年 j_chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
